a1f9f3e6137d27f578cd416d52548b5d5e141ecd
daniel <daniellsmarta@gmail.com>
2019-01-24 16:34:39 +0100

    reward design..

--- schedule_sim/envs/task_day.py
+++ schedule_sim/envs/task_day.py
@@ -1,10 +1,14 @@
 import yaml
 import pathlib
 import numpy as np
+from functools import partial
+
+import gym
 from gym import spaces
 
 from schedule_sim import BaseEnv
 from schedule_sim.tools.parser import yaml_parser
+from schedule_sim.tools.costs import rectified_linear_cost
 
 # A scheduling game assigning aircraft tasks per day
 #
@@ -41,24 +45,20 @@ from schedule_sim.tools.parser import yaml_parser
 
 class TaskDay(BaseEnv):
 
-    def __init__(self, parameters_file, debug=0):
+    def __init__(self, parameters_file, reward_scale=10, debug=0):
         BaseEnv.__init__(self)
         self.parameters = yaml_parser(parameters_file)
-        self.state_space_shape = (self.parameters['aircrafts']['total_number']* \
+        self.observation_space_shape = (self.parameters['aircrafts']['total_number']* \
         self.parameters['tasks']['total_number'],1)
 
-        self.ntasks_per_type = self.distribute_tasks()
+        self.ntasks_per_type, self.cost_fnc_per_type = self.distribute_tasks()
         self.state_trasition_model = self.setup_state_transition_model()
+        self.reward_scale = reward_scale
 
-        self.state_space = spaces.Box(
-            low=0, high=1, shape=self.state_space_shape, dtype=np.float32)
-        self.action_space = spaces.Discrete(1)
-
-        self.step: int = 0
-        self.state = None
-        self.total_return: np.float32 = 0
-        self.prev_reward: np.float32 = 0
-        self.reward: np.float32 = 0
+        self.observation_space = spaces.Box(
+            low=0, high=1, shape=self.observation_space_shape, dtype=np.float32)
+        self.action_space = spaces.Discrete(
+            self.parameters['tasks']['total_number'])
 
         # This should be made with loggers later.....
         if debug:
@@ -70,19 +70,42 @@ class TaskDay(BaseEnv):
 
         self.state -= self.state_trasition_model
         #TODO: Action will actually be more complext than just 0 and 1
+        #TODO: action will probably be the argamax of a Q function, action goes from 1 to N
         self.state[action] = 1
 
+        reward = self.reward_model()
+        import ipdb
+        ipdb.set_trace()
+        done = False
+
+        return np.array(self.state), reward, done, {}
+
     def reset(self):
+        # import ipdb
+        # ipdb.set_trace()
+        self.state = self.np_random.uniform(
+            low=0.8, high=1.0, size=(self.observation_space.shape[0],))
         self.prev_reward = 0
         self.reward = 0
         self.total_return = 0
-        self.state = self.sample_initial_state()
+        return np.array(self.state)
 
-    def sample_initial_state(self):
-        pass
+    def reward_model(self):
+        costs = []
 
-    def reward_model(self, action):
-        pass
+        i = 0
+        for _ in self.cost_fnc_per_type.keys():
+            costs.extend(
+                list(
+                    map(self.cost_fnc_per_type[_],
+                        self.state[i:i + self.ntasks_per_type[_]])))
+            i += self.ntasks_per_type[_]
+
+        assert len(costs) == self.parameters["tasks"]["total_number"]
+
+        import ipdb
+        ipdb.set_trace()
+        return sum(costs) / self.reward_scale
 
     def setup_state_transition_model(self):
         state_transition_model = []
@@ -96,11 +119,24 @@ class TaskDay(BaseEnv):
 
     def distribute_tasks(self):
         tasks_distribution = {}
+        cost_func = {}
         for task_type in self.parameters['tasks']['types'].keys():
             tasks_distribution[task_type] = round(\
             self.parameters['tasks']['types'][task_type]['ratio']*\
             self.parameters['tasks']['total_number'])
-        return tasks_distribution
+
+            linear_cost = self.parameters['tasks']['types'][task_type][
+                'linear_cost_before_due_date']
+
+            step_cost = self.parameters['tasks']['types'][task_type][
+                'step_cost_after_due_date']
+
+            cost_func[task_type] = partial(rectified_linear_cost, linear_cost,
+                                           step_cost)
+
+        assert cost_func.keys() == tasks_distribution.keys(
+        ), "task type with invalid cost input!!"
+        return tasks_distribution, cost_func
 
     def setup_print(self):
         print("====Environment setup complete====")
@@ -119,4 +155,17 @@ if __name__ == '__main__':
     parameteres_default_file = pathlib.Path(
         'config/task_day_custom.yaml').absolute()
 
+    cart = gym.make("CartPole-v0")
+    s0 = cart.reset()
+    print(s0)
+    print("=======")
     env = TaskDay(parameters_file=parameteres_default_file, debug=1)
+
+    state = env.reset()
+    print(state)
+
+    env.step(0)
+    # for _ in range(10):
+    #     next_state = env.step(0)
+    #     print("=======")
+    #     print(next_state)
--- schedule_sim/tools/costs.py
+++ schedule_sim/tools/costs.py
@@ -2,5 +2,21 @@ import math
 import numpy as np
 
 
-def linear_cost():
-    pass
+# Cost should decrease from -1 to 0, then from
+def rectified_linear_cost(linear_cost, max_cost, state):
+    if state < 0:
+        return max_cost
+    elif 0 <= state <= 0:
+        return 0
+    elif 0.1 <= state <= 1:
+        return state * linear_cost
+    else:
+        raise ("STATE IS INVALID!!!")
+
+
+if __name__ == '__main__':
+    from functools import partial
+
+    p = partial(rectified_linear_cost, -1, -10)
+
+    print(p(0.999))
